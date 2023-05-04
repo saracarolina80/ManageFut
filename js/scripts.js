d6461892241b86c6fa7223d0abcdd75dc981575c
saracarolina <sccg@ua.pt>
2023-05-04 12:38:40 +0100

    pagina registo complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,23 @@
 function showModal() {
-  // exibe o fundo modal e o pop-up
-  document.querySelector('.modal-bg').style.display = 'block';
-  document.querySelector('.modal').style.display = 'block';
-  // Desativa os botões de opção após a seleção
+  var modalBg = document.querySelector(".modal-bg");
+  var modal = document.querySelector(".modal");
+  var modalBgP = document.querySelector(".modal-bgP");
+  var modalP = document.querySelector(".modalP");
+
   const radios = document.querySelectorAll('input[type="radio"]');
   radios.forEach((radio) => {
-    radio.addEventListener('change', () => {
+    radio.addEventListener("change", () => {
+      if (radio.value === "player") {
+        // display player popup
+        modalBgP.style.display = "block";
+        modalP.style.display = "block";
+      } else if (radio.value === "coach") {
+        // display coach popup
+        modalBg.style.display = "block";
+        modal.style.display = "block";
+      }
+
+      // Desativa os botões de opção após a seleção
       radios.forEach((r) => {
         r.disabled = true;
       });
@@ -17,6 +29,8 @@ function hideModal() {
   // esconde o fundo modal e o pop-up
   document.querySelector('.modal-bg').style.display = 'none';
   document.querySelector('.modal').style.display = 'none';
+  document.querySelector('.modal-bgP').style.display = 'none';
+  document.querySelector('.modalP').style.display = 'none';
 }
 
 function generateCode(event) {
@@ -29,7 +43,7 @@ function generateCode(event) {
   var code =  num1 + " " + num2 + " " + num3;
 
   // exibe o código em um alerta
-  alert("Seu código é: " + code);
+ // alert("Seu código é: " + code);
 
   // esconde o pop-up e o fundo modal
   hideModal();
@@ -42,3 +56,13 @@ function closeModal() {
   // Redireciona de volta para a página atual
   window.location.href = currentUrl;
 }
+
+ function redirectToPage() {
+    var selectedRole = document.querySelector('input[name="role"]:checked').value;
+
+    if (selectedRole === "player") {
+      window.location.href = "player.html";
+    } else if (selectedRole === "coach") {
+      window.location.href = "coach.html";
+    }
+  }
